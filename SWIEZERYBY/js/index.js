01d9217395903ef1c13e41229c170452ea3f6545
Kurzdor <pavloeksmidt@gmail.com>
2017-06-04 21:56:53 +0200

    Add files via upload

---
+++ SWIEZERYBY/js/index.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:15,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:false
+        }
+    }
+})
+});
