72cb94339b7a004d0f20dc3b48025ab73674cf35
Kurzdor <pavloeksmidt@gmail.com>
2017-06-05 16:23:12 +0200

    Add files via upload

--- SWIEZERYBY/js/index.js
+++ SWIEZERYBY/js/index.js
@@ -1,22 +1,36 @@
 $(document).ready(function(){
-  $('.owl-carousel').owlCarousel({
-    loop:false,
-    margin:15,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
+  $('.slick').slick({
+    prevArrow: '<button class="arrow arrow-left"><picture><source srcset="img/arrow-left.svg"><img src="img/arrow-left.png" alt="arrow-left"></picture></button>',
+    nextArrow: '<button class="arrow arrow-right"><picture><source srcset="img/arrow-right.svg"><img src="img/arrow-right.png" alt="arrow-right"></picture></button>',
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+        {
+        breakpoint: 1024,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4,
+                infinite: true,
+                dots: true
+            }
         },
-        600:{
-            items:2,
-            nav:false
+        {
+        breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
         },
-        1000:{
-            items:4,
-            nav:true,
-            loop:false
+        {
+        breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
         }
-    }
-})
+    ]
+});
 });
